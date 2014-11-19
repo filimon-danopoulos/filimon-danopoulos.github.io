afe46b9e8c327230cf984f108d6fd61a3d4a4eaa
Filimon Danopoulos <filimon.danopoulos@gmail.com>
2014-11-19 08:22:59 +0100

    Added archive JavaScript.

---
+++ js/archive.js
@@ -0,0 +1,31 @@
+(function() {
+    var Handler = (function() {
+        var getArchiveData;
+    
+        function Handler() 
+            this.index = {};
+        };
+        
+        Handler.prototype.init = function() {
+            getArchiveData.call(this);
+        };
+        
+        getArchiveData = function() {
+            $.get("/archive/index.json")
+            .done(function(data) {
+                alert("Loaded data: "+data);
+                this.index = JSON.parse(data);
+            })
+            .fail(function() {
+                alert("Could not load archive data!");
+            });
+        }
+        
+    })();
+    
+    
+    $(document).ready(function() {
+        var handler = new Handler();
+        handler.init();
+    });
+})();
